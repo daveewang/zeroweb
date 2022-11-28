13dd35dc3b10e5628ed80054f601788c0958de23
daveewang <292991944@qq.com>
2022-11-29 00:42:16 +0800

    finish branch part/03

--- route.go
+++ route.go
@@ -2,7 +2,20 @@ package main
 
 import "zeroweb/framework"
 
+// 注册路由规则
 func registerRouter(core *framework.Core) {
-	// core.Get("foo", framework.TimeoutHandler(FooControllerHandler, time.Second*1))
-	core.Get("foo", FooControllerHandler)
+	// 需求1+2:HTTP方法+静态路由匹配
+	core.Get("/user/login", UserLoginController)
+
+	// 需求3:批量通用前缀
+	// Group 方法，它的参数是一个前缀字符串，返回值应该是包含 Get、Post、Put、Delete 方法的一个结构
+	subjectApi := core.Group("/subject")
+	{
+		subjectApi.Post("/add", SubjectAddController)
+		// 需求4:动态路由
+		subjectApi.Delete("/:id", SubjectDelController)
+		subjectApi.Put("/:id", SubjectUpdateController)
+		subjectApi.Get("/:id", SubjectGetController)
+		subjectApi.Get("/list/all", SubjectListController)
+	}
 }
